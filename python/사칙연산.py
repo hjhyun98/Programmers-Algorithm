27435b690162ac2e2535fe860ee41bb6f02134a9
hjhyun98 <hyunjo722@naver.com>
2023-01-27 23:44:35 +0900

    Create 사칙연산.py
    day1

---
+++ python/사칙연산.py
@@ -0,0 +1,19 @@
+# 두 수의 합
+def add(num1, num2):
+    answer = num1 + num2   
+    return answer
+
+# 두 수의 차
+def sub(num1, num2):
+    answer = num1 - num2
+    return answer
+
+# 두 수의 곱
+def mul(num1, num2):
+    answer = num1 * num2
+    return answer
+
+# 두 수의 나누셈 --> 나눗셈 결과의 몫은 //, /은 단순 나눗셈(float형)
+def div(num1, num2):
+    answer = num1 // num2
+    return answer
