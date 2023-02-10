3091e54110965a386aa49005dc43e30538e563a9
hjhyun98 <hyunjo722@naver.com>
2023-02-10 23:56:36 +0900

    day 9, 10

---
+++ python/lv0/day10.py
@@ -0,0 +1,45 @@
+# 점의 위치 구하기
+def solution1(dot):
+    if dot[0] > 0 and dot[1] > 0:
+        return 1
+    elif dot[0] < 0 and dot[1] > 0:
+        return 2
+    elif dot[0] < 0 and dot[1] < 0:
+        return 3
+    else:
+        return 4
+
+def solution1_2(dot):
+    x,y = dot
+    if x*y > 0:
+        return 1 if x > 0 else 3
+    else:
+        return 4 if x > 0 else 2
+    
+# 2차원으로 만들기
+def solution2(num_list, n):
+    answer = []
+    i = 0
+    while i < len(num_list):
+        if (i+1) % n == 0:
+            answer.append(num_list[i-n+1:i+1])
+        i += 1
+    return answer
+
+def solution2_2(num_list, n):
+    answer = []
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i+n])
+    return answer
+
+# 공 던지기
+def solution3(numbers, k):
+    answer = 1
+    for i in range(1,k):
+        answer += 2
+        if answer > len(numbers):
+            answer = answer - len(numbers)
+    return answer
+
+def solution3_2(numbers, k):
+    return numbers[2 * (k - 1) % len(numbers)]
--- python/lv0/day9.py
+++ python/lv0/day9.py
@@ -1,3 +1,5 @@
+import math
+
 #  개미 군단
 def solution1(hp):
     answer = 0
@@ -22,6 +24,7 @@ def solution1_2(hp):
 def solution1_3(hp):
     return hp // 5 + (hp % 5 // 3) + ((hp % 5) % 3)
 
+# 모스 부호
 def solution2(letter):
     morse = { 
     '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
@@ -36,3 +39,52 @@ def solution2(letter):
         answer += morse[i]
     
     return answer
+
+# 가위바위보
+def solution3(rsp):
+    answer = ''
+    for i in rsp:
+        if i == "0":
+            answer += "5"
+        elif i == "2":
+            answer += "0"
+        else:
+            answer += "2"
+    return answer
+
+def solution3_2(rsp):
+    d = {'0':'5', '2':'0', '5': '2'}
+    return ''.join(d[i] for i in rsp)
+
+def solution3_3(rsp):
+    rsp = rsp.replace('2', 's')
+    rsp = rsp.replace('5', 'p')
+    rsp = rsp.replace('0', 'r')
+    rsp = rsp.replace('r', '5')
+    rsp = rsp.replace('s', '0')
+    rsp = rsp.replace('p', '2')
+    return rsp
+
+# 구슬을 나누는 경우의 수
+import math
+def solution4(balls, share):
+    answer = 1
+    i = share+1
+    while i <= balls:
+        answer *= i
+        i += 1
+    return answer/math.factorial(balls-share)
+    
+
+def solution4_2(balls, share):
+    answer = factorial(balls) / (factorial(balls- share)*factorial(share))
+    
+def factorial(n):
+    result =1 
+    for i in range(1, n+1):
+        result = result * i
+    return result
+
+def solution4_3(balls, share):
+    return math.comb(balls, share)
+
